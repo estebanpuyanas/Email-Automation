970a3f7aa152385d65d3619136fb39346d850eec
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2025-03-14 11:27:29 -0400

    saving before major applescript update

--- src/email_automation_script.py
+++ src/email_automation_script.py
@@ -8,27 +8,24 @@ TEMPLATE_PATH = os.path.expanduser("~/Desktop/projects/Email-Automation/email-te
 
 def get_next_monday():
     today = datetime.date.today()
-    days_ahead = (7 - today.weekday()) % 7  # Days until next Monday
+    days_ahead = (7 - today.weekday()) % 7
     next_monday = today + datetime.timedelta(days=days_ahead or 7)
-    return next_monday.strftime("%m/%d/%Y")
+    return next_monday
 
 def generate_email_content(name, company, position):
     with open(TEMPLATE_PATH, "r") as file:
         template = file.read()
-    # Fill placeholders in HTML template
     template = template.replace("{recipient_name}", name)
     template = template.replace("{company}", company)
     template = template.replace("{position}", position)
     return template
 
 def schedule_email(recipient_name, recipient_email, company, position):
-    email_body = generate_email_content(recipient_name, company, position)
-
-    # Escape quotes and special characters for AppleScript
-    email_body = email_body.replace('"', '\\"').replace("\n", "\\n")
+    email_body = generate_email_content(recipient_name, company, position).replace('"', '\\"').replace("\n", "\\n")
 
     sender_email  = "puyanasalazar.e@northeastern.edu"
-    mail_account = "Northeastern University"
+    next_monday = get_next_monday()
+    is_tomorrow = (next_monday - datetime.date.today()).days == 1
 
     apple_script = f'''
     tell application "Mail"
@@ -37,17 +34,26 @@ def schedule_email(recipient_name, recipient_email, company, position):
         tell newMessage
             make new to recipient at end of to recipients with properties {{address:"{recipient_email}"}}
             set sender to theSender
+            delay 0.5
         end tell
+        activate
     end tell
 
     tell application "System Events"
         tell process "Mail"
-            set frontmost to true
-            tell window "Interest in {position} at {company}"
-                click pop up button "Signature:"
+            click button 1 of window 1
+            delay 0.5
+            -- If next Monday is tomorrow, choose "Send Tomorrow at 8:00 AM"
+            if {str(is_tomorrow).lower()} then
+                click menu item 2 of menu 1 of button 1 of window 1
+            else
+                click menu item 3 of menu 1 of button 1 of window 1
                 delay 0.5
-                click menu item "Northeastern" of menu of pop up button "Signature:"
-            end tell
+                -- Fill in date and time for the next Monday at 8:00 AM
+                set value of text field 1 of window 1 to "{next_monday.strftime('%m/%d/%Y')}"
+                set value of text field 2 of window 1 to "8:00 AM"
+                click button "Send" of window 1
+            end if
         end tell
     end tell
     '''
@@ -55,7 +61,6 @@ def schedule_email(recipient_name, recipient_email, company, position):
     subprocess.run(["osascript", "-e", apple_script])
 
 def main():
-
     while True:
         recipient_name = input("Enter recipient's name: ")
         recipient_email = input("Enter recipient's email: ")
